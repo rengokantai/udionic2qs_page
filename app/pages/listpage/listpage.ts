dce862f7761261f5106d9f168fb0e9802a274801
Hernan Y.Ke <yuriqiao@gmail.com>
2016-02-01 22:45:33 -0500

    nothing

--- app/pages/listpage/listpage.ts
+++ app/pages/listpage/listpage.ts
@@ -1,12 +1,15 @@
 /**
  * Created by Hernan Y.Ke on 2016/1/30.
  */
-import {Page, NavController} from 'ionic-framework/ionic';
+import {Page, NavController,NavParams} from 'ionic-framework/ionic';
 
 @Page({
     templateUrl: 'build/pages/listpage/listpage.html'
 })
 
 export class ListPage{
-
+    items;
+    constructor(navParams:NavParams){
+        this.items = navParams.get('items');
+    }
 }
--- app/pages/page4/page4.ts
+++ app/pages/page4/page4.ts
@@ -1,7 +1,7 @@
 /**
  * Created by Hernan Y.Ke on 2016/1/30.
  */
-import {Page, NavController} from 'ionic-framework/ionic';
+import {Page, NavController, NavParams} from 'ionic-framework/ionic';
 import {ListPage} from '../listpage/listpage';
 @Page({
     templateUrl: 'build/pages/page4/page4.html'
@@ -10,10 +10,12 @@ import {ListPage} from '../listpage/listpage';
 export class Page4{
     title = "page";
     nav;
-    constructor(nav: NavController){
+    items;
+    constructor(nav: NavController,navParams:NavParams){
         this.nav = nav;
+        this.items=['A','B','C'];
     }
     addPage(){
-        this.nav.push(ListPage)
+        this.nav.push(ListPage,{items:this.items})
     }
 }
